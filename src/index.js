de02b42214ee0f82438b0f10c234821efb45aee3
Elena Moiseeva <Elena Moiseeva>
2016-11-11 21:41:28 +0300

    task 2B

--- src/index.js
+++ src/index.js
@@ -14,6 +14,22 @@ app.get('/task2A', (req, res) => {
   res.send(sum.toString());
 });
 
+app.get('/task2B', (req, res) => {
+  let str = req.query.fullname;
+  //str = str.replace(/\s/g,"");
+  const arr =  str.trim().split(' ');
+
+  arr.forEach(function(elem, item, array) {
+    array[item] = elem[0] + elem.slice(1)
+  });
+
+  if(arr.length > 3 || str =="" || !(/^[^\d_\\\/]+$/.test(str))){
+    res.end('Invalid fullname');
+  };
+
+  res.end([arr[arr.length-1]].concat(arr.slice(0, arr.length-1).map(el => el.slice(0,1) + ".")).join(" "));
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
